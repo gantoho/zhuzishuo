ef9b3961b09f6d359ee4234398ba6a0cc71477e7
gantoho <i@ganto.icu>
2024-01-18 23:57:54 +0800

    feat(components): tools components add publish btn event, func unfinished

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,5 +1,5 @@
 <script setup lang='ts'>
-import {  } from 'vue'
+import { ref } from 'vue'
 import pencil from '@/assets/icon_pencil_.webp'
 import comment from '@/assets/icon_comment_square_.webp'
 import reload from '@/assets/icon_reload_.webp'
@@ -12,6 +12,18 @@ const themeBtn = ($event) => {
   darkStore.toggleTheme($event)
 }
 
+const dialogRef = ref(null)
+const publish = () => {
+  dialogRef.value.showModal()
+}
+const close = () => {
+  dialogRef.value.close()
+}
+const published = () => {
+  alert('TODO：发布功能未完成')
+  dialogRef.value.close()
+}
+
 const reset = () => {
   window.location.reload()
 }
@@ -20,7 +32,7 @@ const reset = () => {
 <template>
   <div class="tools">
     <div class="tools-start">
-      <img :src="pencil" alt="edit">
+      <img :src="pencil" alt="publish" @click="publish">
       <img :src="comment" alt="info">
     </div>
     <div class="tools-end">
@@ -29,6 +41,13 @@ const reset = () => {
       <img v-else  :src="sun" alt="sun" @click="themeBtn">
     </div>
   </div>
+  <dialog ref="dialogRef" id="dialog">
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <div class="dialog-buttons">
+      <button @click="close">关闭</button>
+      <button @click="published">发布</button>
+    </div>
+  </dialog>
 </template>
 
 <style lang='scss' scoped>
@@ -60,6 +79,32 @@ const reset = () => {
 
       + img {
         margin-left: 20px;
+      }
+    }
+  }
+}
+
+#dialog {
+  background-color: var(--main-bg-color);
+  color: var(--main-text-color);
+  margin: auto;
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid #c0c0c0;
+
+  textarea {
+    background-color: transparent;
+  }
+
+  .dialog-buttons {
+    text-align: right;
+
+    button {
+      background-color: var(--main-bg-color);
+      color: var(--main-text-color);
+
+      + button {
+        margin-left: 10px;
       }
     }
   }
